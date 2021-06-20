db3b994146aaf0b1720590cb98da2079d837709a
DENNIS MWANGI <dennistrevor06@gmail.com>
2021-06-20 19:47:26 +0000

    initial commit

---
+++ Mobile Phone System/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+    Structure customer
+        Dim customername As String
+        Dim address As String
+        Dim postcode As String
+        Dim mobilenumber As String
+
+    End Structure
+
+    Public mycustomerlist As New List(Of customer)
+
+
+
+
+    Public strName, strAddress, strPostcode, strmobileNo, strPlantype As String
+
+    Public ushMinsused, ushTextsSent, ushDataUsed As UShort
+
+End Module
