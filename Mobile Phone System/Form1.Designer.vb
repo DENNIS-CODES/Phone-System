f5546425deea02ae30d1324afbb475c6e3611eea
DENNIS MWANGI <dennistrevor06@gmail.com>
2021-06-20 19:40:42 +0000

    initial commit

---
+++ Mobile Phone System/Form1.Designer.vb
@@ -0,0 +1,387 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCustomerName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPostCode = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtMobileNo = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.cmbPlanType = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtMinsUsed = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtTextsSent = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtDataUsed = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnCustomerDetails = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox2 = New System.Windows.Forms.MaskedTextBox()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(63, 113)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(137, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Customer Name"
+        '
+        'txtCustomerName
+        '
+        Me.txtCustomerName.Location = New System.Drawing.Point(201, 113)
+        Me.txtCustomerName.Name = "txtCustomerName"
+        Me.txtCustomerName.Size = New System.Drawing.Size(204, 20)
+        Me.txtCustomerName.TabIndex = 1
+        Me.txtCustomerName.Text = "Abrar Hamid"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(63, 150)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Addresss"
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(201, 150)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(268, 20)
+        Me.txtAddress.TabIndex = 3
+        Me.txtAddress.Text = "Kingston College"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(63, 196)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(87, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "PostCode"
+        '
+        'txtPostCode
+        '
+        Me.txtPostCode.Location = New System.Drawing.Point(201, 196)
+        Me.txtPostCode.Name = "txtPostCode"
+        Me.txtPostCode.Size = New System.Drawing.Size(60, 20)
+        Me.txtPostCode.TabIndex = 5
+        Me.txtPostCode.Text = "KT1 2AQ"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(265, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(267, 29)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Mobile Phone System"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(66, 232)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(128, 20)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Mobile Number"
+        '
+        'txtMobileNo
+        '
+        Me.txtMobileNo.Location = New System.Drawing.Point(203, 234)
+        Me.txtMobileNo.Name = "txtMobileNo"
+        Me.txtMobileNo.Size = New System.Drawing.Size(123, 20)
+        Me.txtMobileNo.TabIndex = 8
+        Me.txtMobileNo.Text = "071234-456789"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(69, 283)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(87, 20)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Plan Type"
+        '
+        'cmbPlanType
+        '
+        Me.cmbPlanType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbPlanType.FormattingEnabled = True
+        Me.cmbPlanType.Items.AddRange(New Object() {"3G", "4G", "5G"})
+        Me.cmbPlanType.Location = New System.Drawing.Point(203, 285)
+        Me.cmbPlanType.Name = "cmbPlanType"
+        Me.cmbPlanType.Size = New System.Drawing.Size(73, 21)
+        Me.cmbPlanType.TabIndex = 10
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(12, 332)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(123, 20)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Minuites Used"
+        '
+        'txtMinsUsed
+        '
+        Me.txtMinsUsed.Location = New System.Drawing.Point(143, 332)
+        Me.txtMinsUsed.Name = "txtMinsUsed"
+        Me.txtMinsUsed.Size = New System.Drawing.Size(100, 20)
+        Me.txtMinsUsed.TabIndex = 12
+        Me.txtMinsUsed.Text = "0"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(247, 331)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(95, 20)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "Texts Sent"
+        '
+        'txtTextsSent
+        '
+        Me.txtTextsSent.Location = New System.Drawing.Point(348, 331)
+        Me.txtTextsSent.Name = "txtTextsSent"
+        Me.txtTextsSent.Size = New System.Drawing.Size(100, 20)
+        Me.txtTextsSent.TabIndex = 14
+        Me.txtTextsSent.Text = "0"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(454, 332)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(95, 20)
+        Me.Label9.TabIndex = 15
+        Me.Label9.Text = "Data Used"
+        '
+        'txtDataUsed
+        '
+        Me.txtDataUsed.Location = New System.Drawing.Point(583, 340)
+        Me.txtDataUsed.Name = "txtDataUsed"
+        Me.txtDataUsed.Size = New System.Drawing.Size(100, 20)
+        Me.txtDataUsed.TabIndex = 16
+        Me.txtDataUsed.Text = "0"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(90, 451)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(186, 35)
+        Me.btnCalculate.TabIndex = 17
+        Me.btnCalculate.Text = "Calculate Bill"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(535, 458)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(115, 32)
+        Me.btnExit.TabIndex = 18
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.Location = New System.Drawing.Point(628, 27)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(129, 123)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "Kingston Mobiles "
+        '
+        'btnCustomerDetails
+        '
+        Me.btnCustomerDetails.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCustomerDetails.Location = New System.Drawing.Point(309, 451)
+        Me.btnCustomerDetails.Name = "btnCustomerDetails"
+        Me.btnCustomerDetails.Size = New System.Drawing.Size(191, 45)
+        Me.btnCustomerDetails.TabIndex = 20
+        Me.btnCustomerDetails.Text = "Customer Details"
+        Me.btnCustomerDetails.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(203, 66)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(199, 21)
+        Me.ComboBox1.TabIndex = 21
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(369, 232)
+        Me.MaskedTextBox1.Mask = "00000-000 000"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(176, 20)
+        Me.MaskedTextBox1.TabIndex = 22
+        '
+        'MaskedTextBox2
+        '
+        Me.MaskedTextBox2.Location = New System.Drawing.Point(369, 196)
+        Me.MaskedTextBox2.Mask = ">LL00 0LL"
+        Me.MaskedTextBox2.Name = "MaskedTextBox2"
+        Me.MaskedTextBox2.Size = New System.Drawing.Size(176, 20)
+        Me.MaskedTextBox2.TabIndex = 23
+        '
+        'ListView1
+        '
+        Me.ListView1.CheckBoxes = True
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5})
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(729, 123)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(310, 390)
+        Me.ListView1.TabIndex = 24
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Month"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Plan Type"
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Minuites"
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Texts"
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Data"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(496, 386)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(154, 36)
+        Me.Button1.TabIndex = 25
+        Me.Button1.Text = "Update Usage"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1107, 629)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.MaskedTextBox2)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.btnCustomerDetails)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtDataUsed)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtTextsSent)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtMinsUsed)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.cmbPlanType)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtMobileNo)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtPostCode)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtCustomerName)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtCustomerName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtPostCode As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtMobileNo As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents cmbPlanType As ComboBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtMinsUsed As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtTextsSent As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtDataUsed As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents btnCustomerDetails As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents MaskedTextBox2 As MaskedTextBox
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader4 As ColumnHeader
+    Friend WithEvents ColumnHeader5 As ColumnHeader
+    Friend WithEvents Button1 As Button
+End Class
